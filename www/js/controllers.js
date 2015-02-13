ab709176b470e76f140e1c2b3c747afcac460aad
Tony Laidig <dlaidig@mtahq.org>
2015-02-13 11:14:27 -0500

    fix for very annoying default tab behavior. now returns to root for search and favorite tab buttons.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,25 @@
 angular.module('starter.controllers', ['configuration', 'filters'])
+// Controller that makes tabs go to root (for search and favs)
+    .controller('GoHomeCtrl', function($scope, $rootScope, $state, $ionicHistory) {
+        var clearHistory = function(){
+            $ionicHistory.clearHistory();
+            $ionicHistory.nextViewOptions({
+                historyRoot: true
+            });
+        }
+        $scope.goHomeTab = function() {
+            clearHistory();
+            $state.go('tab.home');
+        }
+        $scope.goFavsTab = function() {
+            clearHistory();
+            $state.go('tab.favorites');
+        }
 
+    })
 // Search
-.controller('SearchCtrl', ['$scope', '$location', 'SearchService', '$filter', '$ionicLoading', 'RouteService', '$ionicPopup', '$ionicPlatform', 'SearchesService', 'SHOW_BRANDING',
-	function($scope, $location, SearchService, $filter, $ionicLoading, RouteService, $ionicPopup, $ionicPlatform, SearchesService, SHOW_BRANDING) {
-
+.controller('SearchCtrl', ['$scope', '$rootScope', '$location', 'SearchService', '$filter', '$ionicLoading', 'RouteService', '$ionicPopup', '$ionicPlatform', 'SearchesService', 'SHOW_BRANDING',
+	function($scope, $rootScope,  $location, SearchService, $filter, $ionicLoading, RouteService, $ionicPopup, $ionicPlatform, SearchesService, SHOW_BRANDING) {
 
 		$scope.go = function(path) {
 			$location.path(path);
