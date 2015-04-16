fb64298dcb5ba1f80b484c2db56f3cd6685e7617
Khurshid Fayzullaev <khurshidfayzullaev@yahoo.com>
2015-04-16 13:07:26 -0400

    sync with master branch

--- www/js/filters.js
+++ www/js/filters.js
@@ -22,62 +22,62 @@ angular.module('filters', [])
 
 // modify a link to open URL in a device's browser
 .filter('hrefToJS', function($sce, $sanitize) {
-	return function(text) {
-		return $sce.trustAsHtml($sanitize(text).replace(/href="([\S]+)"/g, "onClick=\"window.open('$1', '_system', 'location=yes')\""));
-	};
+    return function(text) {
+        return $sce.trustAsHtml($sanitize(text).replace(/href="([\S]+)"/g, "onClick=\"window.open('$1', '_system', 'location=yes')\""));
+    };
 })
 
 .filter('isUndefinedOrEmpty', function() {
-	return function(a) {
-		return angular.isUndefined(a) || null === a;
-	};
+    return function(a) {
+        return angular.isUndefined(a) || null === a;
+    };
 })
 
 .filter('isEmptyObject', function() {
-	var bar;
-	return function(obj) {
-		for (bar in obj) {
-			if (obj.hasOwnProperty(bar)) {
-				return false;
-			}
-		}
-		return true;
-	};
+    var bar;
+    return function(obj) {
+        for(bar in obj) {
+            if(obj.hasOwnProperty(bar)) {
+                return false;
+            }
+        }
+        return true;
+    };
 })
 
 .filter('encodeStopName', function() {
-	return function(input) {
+    return function(input) {
         input = input || '';
         return input.replace("/", " & ");
-	};
+    };
 })
 
 .filter('encode', function() {
-	return function(input) {
-		return encodeURIComponent(input);
-	};
+    return function(input) {
+        return encodeURIComponent(input);
+    };
 })
 
 .filter('decode', function() {
-	return function(input) {
-		return decodeURIComponent(input);
-	};
+    return function(input) {
+        return decodeURIComponent(input);
+    };
 })
 
 // always round down to nearest min, do not show time if less than 1 minute away
 .filter('durationView', ['datetimeService',
-	function(datetimeService) {
-		return function(input) {
-			var duration = datetimeService.duration(input);
-			var minutes = duration.minutes;
-			var displayTime = '';
-			if (duration.hours > 0) {
-				minutes = minutes + duration.hours * 60;
-			}
-			if (duration.minutes > 0) {
-				displayTime = minutes + " min";
-			}
-			return displayTime;
-		};
-	}
+    function(datetimeService) {
+        return function(input) {
+            var duration = datetimeService.duration(input);
+            var minutes = duration.minutes;
+            var displayTime = '';
+            if(duration.hours > 0) {
+                minutes = minutes + duration.hours * 60;
+            }
+            if(duration.minutes > 0) {
+                displayTime = minutes + " min";
+            }
+            return displayTime;
+        };
+    }
 ]);
