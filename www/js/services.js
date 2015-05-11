5a2fe57812826a747ef2929d66547f605f1ebd50
Khurshid Fayzullaev <khurshidfayzullaev@yahoo.com>
2015-05-11 09:48:32 -0400

    pass radix to the parseInt() function

--- www/js/services.js
+++ www/js/services.js
@@ -809,7 +809,7 @@ angular.module('starter.services', ['ionic', 'configuration'])
         var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
         var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         var d = R * c * 1000;
-        return parseInt(d);
+        return parseInt(d, 10);
     };
 
     var deg2rad = function(deg) {
