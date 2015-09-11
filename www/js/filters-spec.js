c7311fd4d90c81707504b980bd6f23a66e9719d4
Khurshid Fayzullaev <khurshidfayzullaev@yahoo.com>
2015-09-11 14:11:54 -0400

    Testing: Add tests to filters-spec.js

--- www/js/filters-spec.js
+++ www/js/filters-spec.js
@@ -1,12 +1,34 @@
 ngDescribe({
-  name: 'Filter: encodeStopName',
+  name: 'Filter: encodeStopName filter tests',
   modules: 'filters',
   inject: '$filter',
   tests: function(deps) {
-    it('should be able to replace a forward slash with & character', function () {
+    it('should be able to replace a forward slash with & character', function() {
       var encodeStopName = deps.$filter('encodeStopName');
       expect(encodeStopName('John St/Smith Ave')).toBe('John St & Smith Ave');
       expect(encodeStopName('Adam Dr/Willson Pl')).toBe('Adam Dr & Willson Pl');
+    });
+  }
+});
+
+ngDescribe({
+  name: 'Filter: durationView filter tests',
+  modules: ['atstop'],
+  inject: ['$filter'],
+  tests: function(deps) {
+    it('should not show  times <1 min', function() {
+      var durationView = deps.$filter('durationView');
+      expect(durationView(0)).toBe('');
+    });
+
+    it('should show a time of 10 min', function() {
+      var durationView = deps.$filter('durationView');
+      expect(durationView(633375)).toBe('10 min');
+    });
+
+    it('should properly show times of > 60 min', function() {
+      var durationView = deps.$filter('durationView');
+      expect(durationView(4200922)).toBe('70 min');
     });
   }
 });
