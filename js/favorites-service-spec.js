4a949bc023a32502e01a95b967bfb04d0f93611c
ofureukpebor <ofure.ukpebor@yahoo.com>
2016-12-22 14:38:54 -0500

    Bug fix in favorite-service-test

--- js/favorites-service-spec.js
+++ js/favorites-service-spec.js
@@ -1,46 +1,56 @@
+if (typeof (window) === 'undefined') 
+var loki = require('../www/lib/lokijs/src/lokijs.js');
+
 ngDescribe({
   name: 'Service: Favorites storage tests',
   modules: 'atstop',
   inject: ['FavoritesService'],
   tests: function(favorites) {
-    it('should return list of all favorites', function() {
+
+   beforeEach(function () {    
+      var db = new loki('favoritesDB');
+      var favoriteCollection = db.addCollection('favorites');
+      var id = 'MTA_NYCT_M100';
+      var name = 'M100';
+      var type = 'R';
+      var newFav = {'id':id,'name':name,'type':type};
+      favoriteCollection.insert(newFav);
+      //favorites = db.getCollection('favorites');
+    });
+
+  it('should return list of all favorites', function() {
       favorites.FavoritesService.get().
       then(function(results){
         expect(results.length).not.toBeLessThan(0);
       });
     });
 
-    it('should check if favorite exists or not', function(){
-      var fav_route = 'MTA NYCT_B63';
-      favorites.FavoritesService.inFavorites(fav_route).then(function(response){
-        expect(response).not.toBe(null);
-        expect(response).toBe(true || false);
-      });
-    });
 
     it('should add new favorite to favorites', 
+      function(){
+      var id = 'MTA_NYCT_M100';
+      var name = 'M100';
+      var type = 'R';
+
+      var response = favorites.FavoritesService.add(id, name, type);
+      expect(response).toBe(true);
+
+      favorites.FavoritesService.get().
+      then(function(results){
+        expect(results.length() > 0);
+      });
+
+    });
+
+    it('should remove an existing favorite from favorites', 
       function(){
       var id = 'MTA NYCT_M100';
       var name = 'M100';
       var type = 'R';
-
-      favorites.FavoritesService.add(id, name, type).then(function(response){
-        expect(response).toBeEquals(true);
-      });
-    });
-
-    // it('should remove an existing favorite from favorites', 
-    //   function(){
-    //   var id = 'MTA NYCT_M100';
-    //   var name = 'M100';
-    //   var type = 'R';
-    //   var favorite = {'id':id, 'name':name, 'type': type};
-    //   favorites.FavoritesService.remove(favorite).then(function(response){
-    //     expect(response).toBeEquals(true);
-    //   });
-    // });  
-
-
+      var favorite = {'id':id, 'name':name, 'type': type};
+      var response = favorites.FavoritesService.remove(favorite);
+      expect(response).toBe(true);
+    }); 
 
   }
 });
