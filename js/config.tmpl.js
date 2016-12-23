4d44ba0665f5de6dc6c9e71384dd8b5772c15fd5
ofureukpebor <ofure.ukpebor@yahoo.com>
2016-12-23 10:19:36 -0500

    config.tmpl.js

---
+++ js/config.tmpl.js
@@ -0,0 +1,17 @@
+/*jshint sub:true*/
+
+angular.module('configuration', [])
+    .constant('MAP_TILES', 'http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png')
+    .constant('MAP_ATTRS', 'Map:<a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data:<a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.')
+    .constant('API_END_POINT', '')
+    .constant('API_KEY', '')
+    .constant('MAPBOX_KEY', '')
+    .constant('SHOW_BRANDING', false)
+    .constant('PRIV_POLICY_TEXT','')
+    .constant('BRAND_ABOUT_TEXT','')
+    //FYI: this currently is hard coded into the controller
+    .constant('exampleSearches', {
+        'exampleRoutes': [],
+        'exampleStops': [],
+        'exampleIntersections': []
+    });
